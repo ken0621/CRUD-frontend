71ded308fa0f48561a13d4dcc2ff57d4ba4e4320
ken0621 <markkennethreyes21@gmail.com>
2022-07-22 00:20:52 +0800

    update072220221220ampm

--- src/app/family/front/front.component.ts
+++ src/app/family/front/front.component.ts
@@ -10,26 +10,40 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class FrontComponent implements OnInit {
 
+  data        :any = {};
   constructor(public rest: UserService, private http: HttpClient, private toastr: ToastrService) {
 
   }
   ngOnInit(): void {
+
+    this.data.info="sana all";
   }
 
   test()
   {
-    this.http.post(this.rest.domain + "/api/family/load_family_list",
-    {
-    },
-    {
-    })
-    .subscribe(response => {
+    // this.http.get(this.rest.domain + "/family/load_family_list",
+    // {
+    // },
+    // {
+    // })
+    // .subscribe(response => {
            
 
-    },
-      error => {
-        console.log(error);
-      });
+    // },
+    //   error => {
+    //     console.log(error);
+    //   });
   }
+
+  arrayToQueryString(array_in:any)
+   	{
+       var out = new Array();
+
+       for(var key in array_in){
+           out.push(key + '=' + encodeURIComponent(array_in[key]));
+       }
+
+       return out.join('&');
+   }
 
 }
